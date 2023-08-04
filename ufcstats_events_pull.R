6904de3de56cd6af720746f01eefb7133ee10135
201ben <faybenjamin17@gmail.com>
2023-08-04 18:25:03 -0400

    adding date to events list for joins and var recalc

--- ufcstats_events_pull.R
+++ ufcstats_events_pull.R
@@ -1,6 +1,8 @@
 library(rvest)
 library(dplyr)
 library(data.table)
+library(stringr)
+library(tidyr)
 
 url <- "http://ufcstats.com/statistics/events/completed?page=all"
 
@@ -9,17 +11,24 @@ page <- read_html(url)
 link_data <- page %>%
   html_nodes(".b-link.b-link_style_black") %>%
   html_text() %>%
-  tibble(event_name = .) %>%
-  mutate(event_url = page %>%
-           html_nodes(".b-link.b-link_style_black") %>%
-           html_attr("href"))
-
+  tibble(event_name = .) %>% 
+  mutate(
+    event_url = page %>% 
+      html_nodes(".b-link.b-link_style_black") %>%
+      html_attr("href"),
+    event_date = page %>%
+      html_nodes(".b-statistics__date") %>%
+      html_text() %>% .[-1]
+    )
 
 link_data <- link_data %>% 
-  mutate(event_name = gsub('[[:space:]]{2,}|\\n+', '', event_name))
+  mutate(event_name = gsub('[[:space:]]{2,}|\\n+', '', event_name), 
+         event_date = gsub('[[:space:]]{2,}|\\n+', '', event_date)
+         )
 
 
 url_list <- split(link_data$event_url, link_data$event_name)
+date_list <- split(link_data$event_date, link_data$event_name)
 
 
 separator <- "[[:space:]]{2,}|\\n+"
@@ -38,12 +47,13 @@ for (i in names(url_list)){
     separate(col = Td,into = c('a_td', 'b_td'), sep = separator, extra = "drop") %>% 
     separate(col = Sub,into = c('a_sub', 'b_sub'), sep = separator, extra = "drop") %>% 
     separate(col = Method,into = c('win_method', 'win_desc'), sep = separator, extra = "drop") %>% 
-    mutate(event = i)
+    mutate(event = i,
+           date = date_list[[i]])
   
   dat <- rbindlist(list(dat, tbl)) 
   
-  Sys.sleep(1.5)
+  Sys.sleep(.75)
 }
 
 dat %>% 
-  write.csv(file.path('C:/Users/ben/Desktop', 'ufc_stats822023.csv'))
+  write.csv(file.path('C:/Users/ben/Desktop', 'ufcstats_event.csv'))
